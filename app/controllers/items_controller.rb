65ee7e163237aaeace5f2e2516efbd7ee1369000
john-1997-github <johnjeremielirio@gmail.com>
2021-07-19 15:41:34 +0900

    ユーザー管理機能実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,8 +1,24 @@
 class ItemsController < ApplicationController
   def index
+    @items = Item.all
   end
 
   def new
+    @itmes = Item.new
   end
-  
+
+
+  def create
+    @items =  Item.new(item_params)
+    if @items.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  # def prototype_params
+  #   params.require(:item).permit(:, :, :, :image).merge(user_id: current_user.id)
+  # end
+
 end
---
+++ app/models/item.rb
@@ -0,0 +1,11 @@
+class Item < ApplicationRecord
+  validates :name,  presence: true
+  validates :info,  presence: true
+  validates :price, presence: true
+  validates :image, presence: true
+ 
+  has_one_attached :image
+  belongs_to :user
+  belongs_to :purchase_history
+
+end
