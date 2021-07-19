ee242beec23c5db226dcfcf2af3171e2d46361f5
john-1997-github <johnjeremielirio@gmail.com>
2021-07-19 19:56:40 +0900

    コメントアウトしました

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,20 +1,20 @@
 class ItemsController < ApplicationController
   def index
-    @items = Item.all
+    # @items = Item.all
   end
 
-  def new
-    @itmes = Item.new
-  end
+  # def new
+  #   @itmes = Item.new
+  # end
 
-  def create
-    @items = Item.new(item_params)
-    if @items.save
-      redirect_to root_path
-    else
-      render :new
-    end
-  end
+  # def create
+  #   @items = Item.new(item_params)
+  #   if @items.save
+  #     redirect_to root_path
+  #   else
+  #     render :new
+  #   end
+  # end
 
   # def prototype_params
   #   params.require(:item).permit(:, :, :, :image).merge(user_id: current_user.id)
