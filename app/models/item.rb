2325f172cf233ad967e988c847df6dddbf5339ec
john-1997-github <johnjeremielirio@gmail.com>
2021-07-21 18:44:32 +0900

    itemモデル編集、追加

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,10 +1,31 @@
 class Item < ApplicationRecord
-validates :name,  presence: true
-validates :info,  presence: true
-validates :price, presence: true
-validates :image, presence: true
+with_options presence: true do
+validates :name
+validates :info
+validates :price
+validates :category_id
+validates :sales_status_id
+validates :shipping_fee_status_id
+validates :prefecture_id
+validates :scheduled_delivery_id
+end
+
+validates :image, presence: true, unless: :was_attached?
 
 has_one_attached :image
 belongs_to :user
-belongs_to :purchase_history
+belongs_to :category,numericality: { other_than: 1, message: "can't be blank" } 
+belongs_to :sales_status,numericality: { other_than: 1, message: "can't be blank" } 
+belongs_to :shipping_fee_status,numericality: { other_than: 1, message: "can't be blank" } 
+belongs_to :prefecture,numericality: { other_than: 1, message: "can't be blank" } 
+belongs_to :scheduled_delivery,numericality: { other_than: 1, message: "can't be blank" } 
+# belongs_to :purchase_history
+
+def was_attached?
+  self.image.attached?
 end
+
+end
+
+
+
