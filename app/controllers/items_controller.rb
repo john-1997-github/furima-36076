c1a470f717fa215b37684d27b02571dff05c8373
john-1997-github <johnjeremielirio@gmail.com>
2021-07-22 15:53:33 +0900

    itemコントローラー編集

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,16 +1,15 @@
 class ItemsController < ApplicationController
   def index
-    @items = Item.all
-    @items = Item.order("created_at DESC")
+    #@item = Item.all
   end
 
   def new
-    @itmes = Item.new
+    @item = Item.new
   end
 
   def create
-    @items = Item.new(item_params)
-    if @items.save
+    @item = Item.new(item_params)
+    if @item.save
       redirect_to root_path
     else
       render :new
@@ -18,6 +17,6 @@ class ItemsController < ApplicationController
   end
 
   def item_params
-    params.require(:item).permit(:name, :info, :price, :image, :category_id, :sales_status_id, :shipping_fee_status_id, :prefecture_id, :scheduled_delivery_id, ).merge(user_id: current_user.id)
+    params.require(:item).permit(:name, :info, :price, :image, :category_id, :sales_status_id, :shipping_fee_status_id, :prefecture_id, :scheduled_delivery_id).merge(user_id: current_user.id)
   end
 end
