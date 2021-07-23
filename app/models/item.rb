6efc904ef5adc6d913f5fb93f46057fe41d9f1f2
john-1997-github <johnjeremielirio@gmail.com>
2021-07-23 12:18:37 +0900

    imageをwith_options内に移動

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,6 +2,7 @@ class Item < ApplicationRecord
   with_options presence: true do
     validates :name
     validates :info
+    validates :image
     validates :price, numericality: { only_integer: true, message: 'half-width number' },
                       inclusion: { in: 300..9_999_999, message: 'out of setting range' }, format: { with: /\A[0-9]+\z/ }
     validates :category_id, numericality: { other_than: 0, message: "can't be blank" }
@@ -11,7 +12,6 @@ class Item < ApplicationRecord
     validates :scheduled_delivery_id, numericality: { other_than: 0, message: "can't be blank" }
   end
 
-  validates :image, presence: true
 
   has_one_attached :image
   belongs_to :user
