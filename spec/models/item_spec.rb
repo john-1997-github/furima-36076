e2e3dba6abb1c8b2d058572ba4920ec0fddcc982
john-1997-github <johnjeremielirio@gmail.com>
2021-07-23 15:58:08 +0900

    rspec追加

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -35,7 +35,6 @@ RSpec.describe Item, type: :model do
       it 'カテゴリーの情報が必須であること' do
         @item.category_id = 0
         @item.valid?
-        binding.pry
         expect(@item.errors.full_messages).to include("Category must be other than 0")
       end
 
@@ -87,6 +86,13 @@ RSpec.describe Item, type: :model do
         @item.valid?
         expect(@item.errors.full_messages).to include('Price half-width number')
       end
+
+      it '商品に紐づくUser必須であること' do
+        @item.user = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include("User must exist")
+      end
+
     end
   end
 end
