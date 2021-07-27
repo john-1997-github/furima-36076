8bb3fe600b40073df48f78f30a9aa6c8dea2fdea
john-1997-github <johnjeremielirio@gmail.com>
2021-07-27 14:00:26 +0900

    順番機能設定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,16 +1,16 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index]
   def index
-    @item = Item.all
+    @items = Item.all.order('created_at DESC')
   end
 
   def new
-    @item = Item.new
+    @items = Item.new
   end
 
   def create
-    @item = Item.new(item_params)
-    if @item.save
+    @items = Item.new(item_params)
+    if @items.save
       redirect_to root_path
     else
       render :new
@@ -23,4 +23,13 @@ private
     params.require(:item).permit(:name, :info, :price, :image, :category_id, :sales_status_id, :shipping_fee_status_id,
                                  :prefecture_id, :scheduled_delivery_id).merge(user_id: current_user.id)
   end
+
+
+
+  # def order_item
+  #   if @items.order
+  #     redirect_to root_path
+  #   end
+  # end
+
 end
