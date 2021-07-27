dd282b7831de22cbbb36c595ead9e0906840f56d
john-1997-github <johnjeremielirio@gmail.com>
2021-07-27 17:36:23 +0900

    ログイン有無での表示設定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,7 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, except: [:index]
+  before_action :authenticate_user!, except: [:index, :show]
+  before_action :set_item, only: [:show]
+  
   def index
     @items = Item.order('created_at DESC')
   end
@@ -20,6 +22,10 @@ class ItemsController < ApplicationController
   def show
   end
 
+  # def edit
+  # end
+
+
   private
 
   def item_params
@@ -27,9 +33,16 @@ class ItemsController < ApplicationController
                                  :prefecture_id, :scheduled_delivery_id).merge(user_id: current_user.id)
   end
 
+
+ def set_item
+ @items = Item.find(params[:id])
+ end
+
   # def order_item
   #   if @items.order
   #     redirect_to root_path
   #   end
   # end
+
+
 end
