5d34c9386acc521b636cc1f612c726444faecb94
john-1997-github <johnjeremielirio@gmail.com>
2021-07-28 10:52:54 +0900

    トップページに遷移

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,7 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, except: [:index, :show]
+  before_action :authenticate_user!, except: [:index, :show, :edit]
   before_action :set_item, only: [:show, :update, :edit]
+  before_action :froce_redirect_unless_my_item, only: [:edit, :update]
 
   def index
     @items = Item.order('created_at DESC')
@@ -42,6 +43,13 @@ class ItemsController < ApplicationController
 
   def set_item
     @item = Item.find(params[:id])
+  end
+
+
+  def froce_redirect_unless_my_item
+    if @item.user != current_user
+      redirect_to root_path
+    end
   end
 
   # def order_item
