e5d05868a48c0440679b92737d578c16766d5570
john-1997-github <johnjeremielirio@gmail.com>
2021-08-02 16:50:39 +0900

    購入機能記述

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,11 +1,10 @@
 class Item < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
-  
+
   has_one_attached :image
   belongs_to :user
   has_one    :order
-  
-  
+
   with_options presence: true do
     validates :name
     validates :info
